d855f091c911c1fbd3aa9334966466979a8cb58f
sybapp <sybapp@qq.com>
2018-10-13 19:51:40 +0800

    Trie树实现

---
+++ Trie/src/Trie.java
@@ -0,0 +1,69 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public Map<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private int size;
+    private Node root;
+
+    public Trie() {
+        size = 0;
+        root = new Node();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
